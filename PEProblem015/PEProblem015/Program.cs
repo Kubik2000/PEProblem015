b2cfefc81a6f5f8df66912a88ad13a6cd14686ed
Kubik2000 <jakub.drs@gmail.com>
2015-09-26 15:14:39 +0200

    Upgrade to VS2015 .net 4.6
    Recompiled in Microsoft Visual Studio 2015 Community  with .net
    framework 4.6

---
+++ PEProblem015/PEProblem015/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+/*
+ * Starting in the top left corner of a 2×2 grid, 
+ * and only being able to move to the right and down, 
+ * there are exactly 6 routes to the bottom right corner.
+ * 
+ * How many such routes are there through a 20×20 grid?
+*/
+namespace PEProblem015
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime StartTime = DateTime.Now;
+            
+            ulong[][] result = new ulong[21][];
+            for (int i = 0; i < result.Length; i++)
+            {
+                result[i] = new ulong[21];
+            }
+
+            for (int i = 0; i < result.Length; i++)
+            {
+                result[20][i] = 1ul;
+                result[i][20] = 1ul;
+            }
+
+            for (int i = result.Length - 2; i > -1; i--)
+            {
+                for (int j = result.Length - 2; j > -1; j--)
+                {
+                    result[i][j] = result[i][j + 1] + result[i + 1][j];
+                }
+            }
+            
+            TimeSpan elapsedTime = DateTime.Now - StartTime;
+
+            Console.WriteLine("result: " + result[0][0].ToString());
+            Console.WriteLine("elapsed time: " + elapsedTime.ToString("mm':'ss':'fff") + " mm:ss:fff");
+            Console.ReadLine();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# PEProblem015
+PEProblem015
+============
+This is a project Euler problem number 015
+It has been writen in 
+C#
+Visual Studio Community 2015
+.net framework 4.6
+I strongly suggest to use this only once you have  finished solving the problem.
